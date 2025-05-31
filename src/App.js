396c93be13260bb5bfe6211f52dcded883328c29
Rookie12910 <rakeshdebnath12910@gmail.com>
2025-06-01 00:43:06 +0600

    updated deploy.yml

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useCallback } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import {
